c0cf32e3457a88fa77174559a07d16415cb3d64f
unknown <marlonmazzine@gmai.com>
2020-08-20 18:56:36 -0300

    Mudando para DATABASE_URL_UTF8

--- src/servidor/_criarconexao.js
+++ src/servidor/_criarconexao.js
@@ -15,7 +15,7 @@ const { Client } = require('pg');
 const url = 'postgres://pvbyjombciqqro:a377bd1ab95dda847b2087bc5334535684fe9478a93eb0d2a9b8d712c5213b29@ec2-52-86-116-94.compute-1.amazonaws.com:5432/dfd42ptdsoq6a?useUnicode=true&characterEncoding=UTF-8'
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || url,
+  connectionString: process.env.DATABASE_URL_UTF8 || url,
   ssl: {
     rejectUnauthorized: false
   }
