be662a1e886fd5483146509b7e719af09aad195a
unknown <marlonmazzine@gmai.com>
2020-08-20 22:00:26 -0300

    testando deploy no heroku

--- src/cliente/compras_sem_licitacao/banco/buscar_no_banco/Compras.js
+++ src/cliente/compras_sem_licitacao/banco/buscar_no_banco/Compras.js
@@ -5,6 +5,7 @@ export default class Compras extends React.Component {
         return await fetch(
             'http://localhost:5000/comprassemlicitacao'
         ).then(async res => {
+            console.log('consegui a resposta vou pegá-la')
             const resposta = await res.json()
 
             if (resposta.rowCount === 0) {
@@ -13,6 +14,7 @@ export default class Compras extends React.Component {
 
             return resposta.rows
         }).catch(err => {
+            console.log('não consegui a resposta restornando o erro ' + err)
             return 'Houve um erro ao obter as compras sem licitação do banco. Erro: ' + err.message
         })
     }
